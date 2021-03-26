c20d4c08b74dad5a97347cedfb2f494492e86c2e
Galahad3x <joelaumedes@gmail.com>
2021-03-26 19:17:28 +0100

    modificat settings.py

--- WebProjectSpotify/WebProjectSpotify/settings.py
+++ WebProjectSpotify/WebProjectSpotify/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'web-project-galajat-2021.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'songlist.apps.SonglistConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
