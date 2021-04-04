e4ac8996e2330a4cc925a397a0f09262949728cf
Joe <58695219+joelfc-09@users.noreply.github.com>
2021-04-04 17:37:23 +0200

    Models començats

--- webspotify/models.py
+++ webspotify/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    user_id = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Favourite_Artists(models.Model):
+    user_id = models.CharField(max_length=50)
+    artist_id = models.CharField(max_length=50)
+    term = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Favourite_Genres(models.Model):
+    user_id = models.CharField(max_length=50)
+    genre_name = models.CharField(max_length=50)
+    term = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Favourite_Songs (models.Model):
+    user_id = models.CharField(max_length=50)
+    song_id = models.CharField(max_length=50)
+    term = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
