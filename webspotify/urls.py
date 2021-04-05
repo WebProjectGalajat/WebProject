916eb03e692c84c24a9344ed74b18c49db763e3a
Galahad3x <joelaumedes@gmail.com>
2021-04-05 11:19:03 +0200

    Changed passwords

--- webspotify/urls.py
+++ webspotify/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('', views.main_url),
+	path('', views.main_url, name="index"),
 	path('accounts/', include("django.contrib.auth.urls")),
 	path('shop/', views.shop_url),
-	path('dashboard/', views.dashboard_url)
+	path('dashboard/', views.dashboard_url, name="dashboard")
 ]
