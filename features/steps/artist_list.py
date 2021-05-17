6b8b05935b567b043a2a58caf8206404b2d9ebe1
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-05-17 18:17:14 +0200

    song_list.py falta ficar la URL de llistat de artistes i implementar el @then

--- features/steps/artist_list.py
+++ features/steps/artist_list.py
@@ -1,10 +1,11 @@
 from behave import *
 
+
 @when(u'I list artists')
 def step_impl(context):
-	context.browser.visit(context.get_url('')) #Falta ficar la URL
+    context.browser.visit(context.get_url(''))  # Falta ficar la URL
 
 
 @then(u'the list contains 20 artists')
 def step_impl(context):
-	raise NotImplementedError("ERROR")
+    raise NotImplementedError("ERROR")
--- features/steps/song_list.py
+++ features/steps/song_list.py
@@ -1,8 +1,11 @@
+from behave import *
+
+
 @when(u'I list songs')
 def step_impl(context):
-	raise NotImplementedError(u'STEP: When I list songs')
+    context.browser.visit(context.get_url(''))  # Falta ficar la URL
 
 
 @then(u'the list contains 30 songs')
 def step_impl(context):
-	raise NotImplementedError(u'STEP: Then the list contains 30 songs')
+    raise NotImplementedError(u'STEP: Then the list contains 30 songs')
