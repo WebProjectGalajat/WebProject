910bd23a247b9b5c7eaaff7105b9410e2989ad45
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-05-17 18:44:46 +0200

    add_genre.py parcialment implementat

--- features/steps/add_genre.py
+++ features/steps/add_genre.py
@@ -1,6 +1,16 @@
+from behave import *
+
+
 @when(u'I type a genre')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I type a genre')
+    for row in context.table:
+        context.browser.visit(context.get_url(''))  # Falta ficar la URL
+        if context.browser.url == context.get_url(
+                ''):  # Falta ficar la URL (Aquesta i la anterior URL han de coincidir)
+            form = context.browser.find_by_tag('form').first
+            for heading in row.headings:
+                context.browser.fill(heading, row[heading])
+            form.find_by_value('Submit').first.click()
 
 
 @then(u'the list contains 1 genre')
