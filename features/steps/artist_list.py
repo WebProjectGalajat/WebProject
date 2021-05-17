39a30d4ef35bcfec8a1e6fcd5c7e43b794c2ae48
Galahad3x <joelaumedes@gmail.com>
2021-05-17 19:14:54 +0200

    prova

--- features/steps/artist_list.py
+++ features/steps/artist_list.py
@@ -3,7 +3,7 @@ from behave import *
 
 @when(u'I list artists')
 def step_impl(context):
-    context.browser.visit(context.get_url(''))  # Falta ficar la URL
+    link_to_artists = context.browser.find_elements_by_tag_name('a')
 
 
 @then(u'the list contains 20 artists')
--- features/steps/authentication.py
+++ features/steps/authentication.py
@@ -6,15 +6,15 @@ use_step_matcher("parse")
 @given(u'Exists a user "{user}" with password "{password}"')
 def step_impl(context, user, password):
 	from django.contrib.auth.models import User
-	# User.objects.create_user(username=user, email='user@example.com', password=password)
-	pass
+	context.user = user
+	context.password = password
+	User.objects.create_user(username=user, email='user@example.com', password=password)
 
 
 @given(u'I login as user "{user}" with password "{password}"')
 def step_impl(context, user, password):
-	# context.browser.visit(context.get_url('/accounts/login/'))
-	# form = context.browser.find_by_tag('form').first
-	# context.browser.fill('username', user)
-	# context.browser.fill('password', password)
-	# form.find_by_value('login').first.click()
-	pass
+	context.browser.visit(context.get_url('/accounts/login/'))
+	form = context.browser.find_elements_by_tag_name('form')[0]
+	context.browser.fill('username', user)
+	context.browser.fill('password', password)
+	form.find_elements_by_value('login')[0].click()
