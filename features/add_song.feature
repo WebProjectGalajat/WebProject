eb093db39756f9e4bc6398b11f2af6697c5bc733
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-05-19 11:39:12 +0200

    add_song.py fet falta provar

--- features/add_song.feature
+++ features/add_song.feature
@@ -1,20 +1,14 @@
 Feature: Afegir cançó
-    In order to add a song,
-    As a user,
-    I want to search and select a song.
+  In order to add a song,
+  As a user,
+  I want to search and select a song.
 
-    Background: There is a registered user
-        Given Exists a user "user" with password "password"
+  Background: There is a registered user
+    Given Exists a user "user" with password "password"
 
-    Scenario: Add a song
-        Given I login as user "user" with password "password"
-        When I type a song that is correct
-        Then I'm viewing a list containing
-            | name              |
-            | Hotline Bling     |
-        And the list contains 1 songs
-
-    Scenario: Add a song that does not exist
-        Given I login as user "user" with password "password"
-        When I type a song that does not exist
-        Then I get an error saying it doesn't exist
+  Scenario: Add a song
+    Given I login as user "user" with password "password"
+    When I type a song that is correct
+    Then I'm viewing a list containing 1 song
+      | name          |
+      | Hotline Bling |
--- features/steps/add_song.py
+++ features/steps/add_song.py
@@ -1,13 +1,23 @@
+from selenium.webdriver import ActionChains
+
+
 @when(u'I type a song that is correct')
+def step_impl(context, song):
+    context.browser.get(context.get_url('songs/'))
+    form = context.browser.find_element_by_tag_name('form')
+    name_box = form.find_element_by_id('id_name')
+    name_box.send_keys(song)
+    description_box = form.find_element_by_id('id_description')
+    description_box.send_keys('Descripcio original')
+    submit_button = form.find_element_by_tag_name('input')[-1]
+    ActionChains(context.browser).click(submit_button).perform()
+
+
+@then(u'I\'m viewing a list containing 1 song')
 def step_impl(context):
-	raise NotImplementedError(u'STEP: When I type a song that is correct')
-
-
-@then(u'the list contains 1 songs')
-def step_impl(context):
-	raise NotImplementedError(u'STEP: Then the list contains 1 songs')
-
-
-@when(u'I type a song that does not exist')
-def step_impl(context):
-	raise NotImplementedError(u'STEP: When I type a song that does not exist')
+    context.browser.get(context.get_url('/songs/'))
+    divs = context.browser.find_element_by_tag_name('div')
+    for div in divs:
+        if div.id == "content":
+            tags = div.find_element_by_tag_name('li')
+            assert len(tags) > 1
