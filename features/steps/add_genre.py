8fc91e3a85f54ca16058ad03ade9afbcf92f628a
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-05-19 11:13:18 +0200

    add_genre.py parcialment implementat v2.0

--- features/steps/add_genre.py
+++ features/steps/add_genre.py
@@ -1,16 +1,17 @@
-from behave import *
+from selenium.webdriver import ActionChains
 
 
 @when(u'I type a genre')
-def step_impl(context):
-    for row in context.table:
-        context.browser.visit(context.get_url(''))  # Falta ficar la URL
-        if context.browser.url == context.get_url(
-                ''):  # Falta ficar la URL (Aquesta i la anterior URL han de coincidir)
-            form = context.browser.find_by_tag('form').first
-            for heading in row.headings:
-                context.browser.fill(heading, row[heading])
-            form.find_by_value('Submit').first.click()
+def step_impl(context, genre):
+    context.browser.get(context.get_url(''))  # Falta la URL
+    form = context.browser.find_element_by_tag_name('form')
+    name_box = form.find_element_by_id('id_genre')
+    name_box.send_keys(genre)
+    context.genre = genre
+    description_box = form.find_element_by_id('id_description')
+    description_box.send_keys('Descripcio original')
+    submit_button = form.find_element_by_tag_name('input')[-1]
+    ActionChains(context.browser).click(submit_button).perform()
 
 
 @then(u'the list contains 1 genre')
