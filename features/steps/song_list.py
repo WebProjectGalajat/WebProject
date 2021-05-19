5397fa9e38cb944fe1c156d7701fba9d6720dd86
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-05-19 11:45:29 +0200

    song_list.py fet falta provar

--- features/steps/song_list.py
+++ features/steps/song_list.py
@@ -3,9 +3,13 @@ from behave import *
 
 @when(u'I list songs')
 def step_impl(context):
-    context.browser.visit(context.get_url(''))  # Falta ficar la URL
+    context.browser.visit(context.get_url('/songs'))
 
 
 @then(u'the list contains 30 songs')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the list contains 30 songs')
+    divs = context.browser.find_elements_by_tag_name('div')
+    for div in divs:
+        if div.id == "content":
+            tags = div.find_elements_by_tag_name('li')
+            assert len(tags) > 1
